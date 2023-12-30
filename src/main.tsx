2e8916b3e96ee227daaa5cbf70fbed5f43fd7952
nilsreich <nilsreich@gmail.com>
2023-12-30 14:15:12 +0000

    Update tsconfig.json, vite.config.ts, and index.html Delete App.css, App.tsx, index.css, and public/vite.svg Add dependencies and devDependencies in package.json Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,53 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./pages/App.tsx";
+import { About } from "./pages/About.tsx";
+import { Navbar } from "./components/Navbar.tsx";
+import "./globals.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import {
+  Outlet,
+  RouterProvider,
+  Router,
+  Route,
+  RootRoute,
+} from "@tanstack/react-router";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootRoute = new RootRoute({
+  component: () => (
+    <div className="h-svh	flex flex-col ">
+      <Navbar />
+      <Outlet />
+    </div>
+  ),
+});
+
+const indexRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: App,
+});
+
+const aboutRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: "/about",
+  component: About,
+});
+
+const routeTree = rootRoute.addChildren([indexRoute, aboutRoute]);
+
+const router = new Router({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/pages/App.tsx
@@ -0,0 +1,5 @@
+import { count } from "@/lib/signals";
+
+export const App = () => {
+  return <div className="">MainPage - {count}</div>;
+};
